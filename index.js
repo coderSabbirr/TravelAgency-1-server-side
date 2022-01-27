47ec9428e9bed987029742f9bb1604033cdb670b
coderSabbirr <sabirnahid5817@gmail.com>
2022-01-27 14:36:24 +0600

    api upadte

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
 require('dotenv').config()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 const fileUpload = require('express-fileupload')
 
 app.use(cors())
 app.use(express.json());
 app.use(fileUpload());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xohwd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://jasco_data:fJMiSq91GXthOxY6@cluster0.mvih6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -21,54 +21,106 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const database = client.db("uniqueShop");
-        const productsCollection = database.collection("products");
+        const database = client.db("travel-agency");
+        const blogsCollection = database.collection("blogs");
         const addCartCollection = database.collection("addcart");
-
+        const wishlistCollection = database.collection("wishlist");
         const reviewsCollection = database.collection("reviews");
         const usersCollection = database.collection('users');
-
+        const ordersCollection = database.collection("orders");
 
 
 
         //POST API for  new products
-        app.post("/products", async (req, res) => {
+        app.post("/blogs", async (req, res) => {
             const title = req.body.title;
-            const Categories = req.body.Categories;
-            const desc1 = req.body.desc1;
-            const desc2 = req.body.desc2;
-            const desc3 = req.body.desc3;
-            const desc4 = req.body.desc4;
-            const price = req.body.price;
+            const subtitle = req.body.subtitle;
+            const dec = req.body.dec;
+            const cost = req.body.cost;
+            const Location = req.body.Location;
+            const category = req.body.category;
+            const rating = req.body.rating;
+            const status = req.body.status;
+            const useremail = req.body.useremail;
             const image = req.files.image;
-
             const imageData = image.data;
             const encodedData = imageData.toString('base64')
             const imgBuffer = Buffer.from(encodedData, 'base64')
             const data = {
-                title, desc3, desc4, price, desc1, desc2, Categories,
+                title, subtitle, dec, category, image, cost, Location, status, rating, useremail,
                 image: imgBuffer
             }
-            const result = await productsCollection.insertOne(data);
+            const result = await blogsCollection.insertOne(data);
             res.json(result);
         })
 
+        app.put('/blogs/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const title = req.body.title;
+            const subtitle = req.body.subtitle;
+            const dec = req.body.dec;
+            const cost = req.body.cost;
+            const Location = req.body.Location;
+            const category = req.body.category;
+            const rating = req.body.rating;
+            const username = req.body.username;
+            const image = req.files.image;
+            const imageData = image.data;
+            const encodedData = imageData.toString('base64')
+            const imgBuffer = Buffer.from(encodedData, 'base64')
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    title: title,
+                    subtitle: subtitle,
+                    dec:dec,
+                    cost:cost,
+                    Location: Location,
+                    category: category,
+                    rating: rating,
+                    username:username,
+                    image:imgBuffer
+                }
+            };
+            const result = await blogsCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+        app.put('/status/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const status = req.body.status;
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    status:status              }
+            };
+            const result = await blogsCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
         //GET API for all the products\ showing UI
-        app.get("/products", async (req, res) => {
-            const result = productsCollection.find({});
-            const products = await result.toArray();
-            res.send(products);
+        app.get("/blogs", async (req, res) => {
+            const result = blogsCollection.find({});
+            const blogs = await result.toArray();
+            res.send(blogs);
+
         })
+        app.get("/blogs/:status", async (req, res) => {
+            const result = await blogsCollection.find({
+                status: req.params.status,
+            }).toArray();
+            res.send(result);
+        });
         //Get API for certain product by id
-        app.get("/products/:id", async (req, res) => {
-            const productDetails = await productsCollection.findOne({ _id: ObjectId(req.params.id) });
-            res.send(productDetails)
+        app.get("/blogs/:id", async (req, res) => {
+            const blogs = await blogsCollection.findOne({ _id: ObjectId(req.params.id) });
+            res.send(blogs)
 
         });
         //Delete API- delete products
-        app.delete('/products/:id', async (req, res) => {
-            const deletedProducts = await productsCollection.deleteOne({ _id: ObjectId(req.params.id) });
-            res.json(deletedProducts)
+        app.delete('/blogs/:id', async (req, res) => {
+            const blogsDelete = await blogsCollection.deleteOne({ _id: ObjectId(req.params.id) });
+            res.json(blogsDelete)
         });
 
         //POST API- all users siging with email
@@ -94,6 +146,7 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.json(result);
         });
+        
         app.get('/users/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email: email };
@@ -106,56 +159,25 @@ async function run() {
         })
 
         //POST API for Products order
-
+      
 
         //GET API-orders 
-
+   
+       
         //Delete API- delete order
-  
+        app.delete('/orders/:id', async (req, res) => {
+            const deletedOrder = await ordersCollection.deleteOne({ _id: ObjectId(req.params.id) });
+            res.json(deletedOrder)
+        });
+
         //Update order status api
-        app.put('/orders/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: ObjectId(id) }
-            const updateStatus = req.body;
-            const options = { upsert: true }
-            const updateDoc = {
-                $set: {
-                    status: updateStatus
-                }
-            };
-            const result = await ordersCollection.updateOne(filter, updateDoc, options);
-            res.json(result);
-
+        
+        app.get("/wishlist/:email", async (req, res) => {
+            const result = await wishlistCollection.find({
+                user_email: req.params.email,
+            }).toArray();
+            res.send(result);
         });
-
-        //POST API for Products order
-        app.get("/adminblogs", async (req, res) => {
-            const result = addCartCollection.find({});
-            const addCart = await result.toArray();
-            res.send(addCart);
-        })
-
-        app.post('/userblogs', async (req, res) => {
-            const addCart = await addCartCollection.insertOne(req.body);
-            res.json(addCart);
-        });
-
-        app.delete('/addcart/:id', async (req, res) => {
-            const deletedaddcart = await addCartCollection.deleteOne({ _id: ObjectId(req.params.id) });
-            res.json(deletedaddcart)
-        });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
